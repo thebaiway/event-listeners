83d634b52607bbe2d4e84e1d3693c31a59a3806b
Bailee <bailee.roby@gmail.com>
2023-01-23 22:40:50 -0800

    uploading files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  function handleClick() {
+    console.log("I was clicked!")
+  }
+
+  function mouseHover () {
+    console.log("I was hovered!")
+  }
+
+  return (
+    <div className="container">
+         <img onMouseEnter = {mouseHover} src="https://picsum.photos/640/360" alt = ""/>
+          <button onClick = {handleClick}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
